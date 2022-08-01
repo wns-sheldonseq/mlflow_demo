cdc78421d4df892b95e8c2f45f3cd9e8840c44f4
wns-sheldonseq <106670660+wns-sheldonseq@users.noreply.github.com>
2022-08-01 16:04:59 +0530

    Update train.py

--- files_for_training_model/train.py
+++ files_for_training_model/train.py
@@ -16,20 +16,38 @@ and the percentage of people with heart disease in an imaginary sample of 500 to
 
 
 """
+import os
+import warnings
+import sys
 
 import pandas as pd
-import seaborn as sns
 import numpy as np
+#import seaborn as sns
+import matplotlib.pyplot as plt
 import pickle
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import logging
+
+#mlflow.set_tracking_uri("http://10.31.8.38:5000")
+mlflow.set_experiment("/Users/u392578@wns.com/Exp_01")
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
 
 df = pd.read_csv('heart_data.csv')
-print(df.head())
+#print(df.head())
 
 df = df.drop("Unnamed: 0", axis=1)
 #A few plots in Seaborn to understand the data
 
-sns.lmplot(x='biking', y='heart.disease', data=df)  
-sns.lmplot(x='smoking', y='heart.disease', data=df)  
+#sns.lmplot(x='biking', y='heart.disease', data=df)  
+#sns.lmplot(x='smoking', y='heart.disease', data=df)  
 
 
 x_df = df.drop('heart.disease', axis=1)
@@ -47,27 +65,38 @@ pickle.dump(X_train, open(filename, 'wb'))
 
 from sklearn import linear_model
 
+with mlflow.start_run(experiment_id="4281740375320786"):
 #Create Linear Regression object
-model = linear_model.LinearRegression()
+    model = linear_model.LinearRegression()
 
 #Now let us call fit method to train the model using independent variables.
 #And the value that needs to be predicted (Images_Analyzed)
 
-model.fit(X_train, y_train) #Indep variables, dep. variable to be predicted
-print(model.score(X_train, y_train))  #Prints the R^2 value, a measure of how well
+    model.fit(X_train, y_train) #Indep variables, dep. variable to be predicted
+    print(model.score(X_train, y_train))  #Prints the R^2 value, a measure of how well
 
 
-prediction_test = model.predict(X_test)    
-print(y_test, prediction_test)
-print("Mean sq. errror between y_test and predicted =", np.mean(prediction_test-y_test)**2)
+    prediction_test = model.predict(X_test)    
+    print(y_test, prediction_test)
+    print("Mean sq. errror between y_test and predicted =", np.mean(prediction_test-y_test)**2)
 
-import pickle
-pickle.dump(model, open('model.pkl','wb'))
+    pickle.dump(model, open('model.pkl','wb'))
 
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[20.1, 56.3]]))
+    model = pickle.load(open('model.pkl','rb'))
+    #print(model.predict([[20.1, 56.3]]))
 
+    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
+        # Model registry does not work with file store
+    if tracking_url_type_store != "file":
+
+            # Register the model
+            # There are other ways to use the Model Registry, which depends on the use case,
+            # please refer to the doc for more information:
+            # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+        mlflow.sklearn.log_model(model, "model", registered_model_name="Linear-Regression")
+    else:
+        mlflow.sklearn.log_model(model, "model")
 #Model is ready. Let us check the coefficients, stored as reg.coef_.
 #These are a, b, and c from our equation. 
 #Intercept is stored as reg.intercept_
